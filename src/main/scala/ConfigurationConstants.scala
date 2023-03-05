79eafe26b63aff405f3ff463038a1f75fc08e5cc
Miro Keimiöniemi <miro.keimioniemi@aalto.fi>
2023-03-05 14:36:26 +0200

    Implemented the TerrainTile trait with a couple of example terrain tile classes implementing it with different degrade methods based on their nature and ConfigurationConstants. Also created and added the progress_log.txt file.

--- src/main/scala/ConfigurationConstants.scala
+++ src/main/scala/ConfigurationConstants.scala
@@ -1,9 +1,16 @@
 /** Contains the current configuration of the game as constants specifying class attributes that are initialized by constant literals.
- * 
+ *
  * Most values should generally range between 0 and 100 for the gameplay to make sense but there is no explicit limitation for it. */
 
 /** TerrainTiles' attribute values are interpreted as percentages such that 100 -> 100% */
 // Grass
+val GrassFlatness = 90
+val GrassSolidity = 90
+val GrassVegetationDensity = 5
+val GrassElevation = 0
 
 // Rock
-
+val RockFlatness = 5
+val RockSolidity = 100
+val RockVegetationDensity = 0
+val RockElevation = 100
--- src/main/scala/TerrainTile.scala
+++ src/main/scala/TerrainTile.scala
@@ -1,14 +1,47 @@
+import scala.math.max
+import scala.math.min
+
 trait TerrainTile:
+
+  var flatness: Int
+  var solidity: Int
+  var vegetationDensity: Int
+  var elevation: Int
+
+  def degrade(damage: Int): Unit
 
 end TerrainTile
 
 class GrassTile extends TerrainTile:
-  
+
+  var flatness = GrassFlatness
+  var solidity = GrassSolidity
+  var vegetationDensity = GrassVegetationDensity
+  var elevation = GrassElevation
+
+  // TODO: Refactor the method to not contain any magic numbers ------------- !
+  def degrade(damage: Int) =
+    flatness = max(0, flatness - damage)
+    solidity = max(0, solidity - damage)
+    vegetationDensity = 0
+    elevation = 0
+  end degrade
+
 end GrassTile
 
 class RockTile extends TerrainTile:
+
+  var flatness = RockFlatness
+  var solidity = RockSolidity
+  var vegetationDensity = RockVegetationDensity
+  var elevation = RockElevation
   
+  // TODO: Refactor the method to not contain any magic numbers ------------- !
+  def degrade(damage: Int) =
+    flatness = min(100, flatness + damage)
+    solidity = max(0, solidity - (damage / 10))
+    vegetationDensity = 0
+    elevation = max(0, elevation - damage)
+  end degrade
+
 end RockTile
-
-
-  
